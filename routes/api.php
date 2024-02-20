12448a87e162642e65fa18954bdcb963343875c6
cikotgr <cikotegar.s@gmail.com>
2024-02-20 12:08:45 +0700

    [fix] kernel permission

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,7 @@ use App\Http\Controllers\UserController;
 */
 
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/all-questions', [QuestionController::class, 'index']);
