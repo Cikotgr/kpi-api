ce1feac91fa0b5285066ed8b64fb956835d5746c
cikotgr <cikotegar.s@gmail.com>
2024-02-20 09:49:25 +0700

    [feat] add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,18 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        $role = ['admin', 'staf', 'ob'];
+        foreach ($role as $key => $value) {
+            Role::create(['name' => $value]);
+        }
+        
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+        ])->assignRole('admin');
+
+
     }
 }
