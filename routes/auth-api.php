745c8dd51e72ac68db3ef53b9c5724b4e95fad8c
cikotgr <cikotegar.s@gmail.com>
2024-02-20 11:19:20 +0700

    [fix] auth with sanctum

--- routes/auth-api.php
+++ routes/auth-api.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-require __DIR__.'/auth-api.php';
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
