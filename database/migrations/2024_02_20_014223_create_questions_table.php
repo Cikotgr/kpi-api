1377f1d459bbe644f68f28f95661dda6f6ca319b
cikotgr <cikotegar.s@gmail.com>
2024-02-22 08:57:23 +0700

    [fix] isiPertanyaan

--- database/migrations/2024_02_20_014223_create_questions_table.php
+++ database/migrations/2024_02_20_014223_create_questions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('isiPertanyaan');
+            $table->json('isiJawaban');
             $table->timestamps();
         });
     }
